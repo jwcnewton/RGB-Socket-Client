3953fb629d9cb1f1fcffacc709f5941af303710a
jwcnewton <jwcnewton@gmail.com>
2017-11-24 09:59:15 +0000

    More fixes

--- server.js
+++ server.js
@@ -1,16 +1,48 @@
 const io = require('socket.io-client');
 const socketUrl = 'https://rgb-socket-api.herokuapp.com/';
-var socket = io.connect(socketUrl, {reconnect: true});
-const rgbHandler = require('./src/rgbHandler.js');
+var socket = io.connect(socketUrl, { reconnect: true });
+var five = require("johnny-five");
+var led = null;
 
-socket.on('connect', function(){
-    console.log('Client connected');
+five.Board().on("ready", function () {
+
+    socket.on('connect', function () {
+        console.log('Client connected');
+    });
+
+    socket.on('rgb-pull', function (data) {
+        changeColour(data.text);
+    });
+
+    socket.on('disconnect', function () {
+        console.log('Client disconnected');
+    });
+
+    led = new five.Led.RGB({
+        pins: {
+            red: 6,
+            green: 5,
+            blue: 3
+        }
+    });
+
+    // Turn it on and set the initial color
+    led.on();
+    led.color("#FF0000");
 });
 
-socket.on('rgb-pull', function(data){
-    rgbHandler.changeColour(data.text);
-});
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
 
-socket.on('disconnect', function(){
-    console.log('Client disconnected');
-});
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+let changeColour = (colour) => {
+    let hexColour = rgbToHex(colour[0], colour[1], colour[2]);
+    console.log(hexColour);
+    led.color(hexColour);
+    led.pulse(500);
+}
