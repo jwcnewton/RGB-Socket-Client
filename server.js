2e3286c04ec58312abaa65b66281e56bcca62dd4
jwcnewton <jwcnewton@gmail.com>
2017-11-24 09:46:47 +0000

    Fixed socket error

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const socket = require('socket.io-client');
 const socketUrl = 'https://rgb-socket-api.herokuapp.com/';
+var socket = io.connect(socketUrl, {reconnect: true});
 const rgbHandler = require('./src/rgbHandler.js');
-socket.io(socketUrl);
 
 socket.on('connect', function(){
     console.log('Client connected');
